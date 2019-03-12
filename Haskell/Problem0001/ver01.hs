d203d627681595bcfb60e33fb07e02487de110eb
DSCF_1224 <37315588+DSCF-1224@users.noreply.github.com>
2019-03-12 12:25:56 +0900

    2019.03.12 12:00
    [1/3] update Haskell\Problem0001\ver01.hs
    [2/3] add Haskell\Problem0001\compile_ver01.bat
    [3/3] update .gitignore

--- Haskell/Problem0001/ver01.hs
+++ Haskell/Problem0001/ver01.hs
@@ -3,6 +3,9 @@
 -- [Used Compiler]
 -- The Glorious Glasgow Haskell Compilation System, version 8.6.3
 
+-- modules to import
+import System.Exit
+
 -- Integer型変数`n`が次の2つの条件の少なくとも一方を満足するとき、`n`自身を返します。
 -- 1. 整数`n`が3の倍数であるとき。
 -- 2. 整数`n`が5の倍数であるとき。
@@ -17,17 +20,19 @@ is_TargetMultiple n
 -- Integer型変数`n`が3未満のとき、`0`を返します。
 prob0001_core :: Integer -> Integer
 prob0001_core n
-    | n >= 4 = is_TargetMultiple (n) + prob0001_core (n - 1)
-    | n == 3 = 3
-    | n <= 2 = 0
+    | n <= 2    = 0
+    | n == 3    = 3
+    | otherwise = is_TargetMultiple (n) + prob0001_core (n - 1)
 
 -- 再帰処理の範囲の調整
 prob0001_main :: Integer -> Integer
 prob0001_main n = prob0001_core (n - 1)
 
 -- 以下、主プロセス --
+main :: IO ()
 main = do
-    print $ prob0001_main (    10) -- 23
-    print $ prob0001_main (  1000) -- 233168
-    print $ prob0001_main (100000) -- 2333316668
+    print $ prob0001_main     10 -- 23
+    print $ prob0001_main   1000 -- 233168
+    print $ prob0001_main 100000 -- 2333316668
     putStrLn "Finished" 
+    exitWith $ ExitSuccess
