ab6f28a514d8ddfd27b318b1a88b14331d619f29
DSCF_1224 <37315588+DSCF-1224@users.noreply.github.com>
2019-08-21 07:12:04 +0900

    2019.08.21 AM07:00
    [1/1] update the sample codes
    1. JuliaLang\Problem0011\0011_01.jl

--- JuliaLang/Problem0011/0011_01.jl
+++ JuliaLang/Problem0011/0011_01.jl
@@ -32,14 +32,14 @@ function problem0011(path_datafile::Core.AbstractString, len_calc::Core.Integer)
 
 	# STEP.04
 	# read out the given data as `Core.Array{Core.String, 1}`
-	for itr_line = 1:1:num_lines
+	for itr_line = 1 : 1 : num_lines
 		given_array[1:end, itr_line] = Base.parse.( Core.UInt32, Base.split( Base.readline(iostream_datafile), " " ) )
 	end
 
 	val_prod_max = Base.zero(Core.UInt32)
 
-	for itr_rw in 1 : 1 : num_lines - len_calc + 1
-	for itr_cl in 1 : 1 : num_lines - len_calc + 1
+	for itr_rw in 1 : 1 : num_lines
+	for itr_cl in 1 : 1 : num_lines
 
 		val_prod_hor  = given_array[itr_rw, itr_cl] # horizontal
 		val_prod_vrt  = given_array[itr_rw, itr_cl] # vertiacal
@@ -47,8 +47,40 @@ function problem0011(path_datafile::Core.AbstractString, len_calc::Core.Integer)
 		val_prod_slpr = given_array[itr_rw, itr_cl] # sploed. right
 
 		for itr_elm in 1 : 1 : len_calc - 1
+
+			itr_rw_pls = itr_rw + itr_elm
+			itr_cl_pls = itr_cl + itr_elm
+			itr_rw_mns = itr_rw - itr_elm
+			itr_cl_mns = itr_cl - itr_elm
+
+			if itr_cl_pls <= num_lines
+				val_prod_hor *= given_array[itr_rw, itr_cl_pls]
+			else
+				val_prod_hor *= Base.zero(Core.UInt32)
+			end
+
+			if itr_rw_pls <= num_lines
+				val_prod_vrt *= given_array[itr_rw_pls, itr_cl]
+			else
+				val_prod_vrt *= Base.zero(Core.UInt32)
+			end
+
+			if itr_rw_pls <= num_lines && itr_cl_pls <= num_lines
+				val_prod_slpl *= given_array[itr_rw_pls, itr_cl_pls]
+			else
+				val_prod_slpl *= Base.zero(Core.UInt32)
+			end
+
+			if itr_rw_mns >= 1 && itr_cl_mns >= 1
+				val_prod_slpr *= given_array[itr_rw_mns, itr_cl_mns]
+			else
+				val_prod_slpr *= Base.zero(Core.UInt32)
+			end
+
 		end
-	
+
+		val_prod_max = Base.max(val_prod_max, val_prod_hor, val_prod_vrt, val_prod_slpl, val_prod_slpr)
+
 	end
 	end
 
@@ -58,7 +90,7 @@ function problem0011(path_datafile::Core.AbstractString, len_calc::Core.Integer)
 
 	# STEP.05
 	# set the return value of this <function>
-	return Core.Nothing
+	return val_prod_max
 
 end
 
